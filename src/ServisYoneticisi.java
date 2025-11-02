9f5a1a7817a7c574000c74034cb8c29e2614a135
Ali <106073110+Zerothlaw780@users.noreply.github.com>
2025-11-02 18:56:46 +0300

    Update ServisYoneticisi.java

--- src/ServisYoneticisi.java
+++ src/ServisYoneticisi.java
@@ -1,2 +1,9 @@
 public class ServisYoneticisi {
+  	public void cihazEkle() {
+		Cihaz cihaz= new Cihaz();
+		cihaz.setArizaTanimi(null);
+		cihaz.setSeriNo(null);
+		cihaz.setMarkaModel(null);
+		
+		
 }
