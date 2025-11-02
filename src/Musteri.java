5a59a14954a23aa39e34992a06811cdba86800ff
Muhammed Veli NAMLI <muhammedveli.namli@icloud.com>
2025-11-02 16:18:03 +0300

    created: Musteri.java created.

---
+++ src/Musteri.java
@@ -0,0 +1,38 @@
+public class Musteri {
+
+    private int musteriId;
+    private String adSoyad;
+    private String telefon;
+    private String adres;
+    private String mail;
+
+    public int getMusteriId() {
+        return musteriId;
+    }
+    public void setAdSoyad(){
+        this.adSoyad = adSoyad;
+    }
+    public void setTelefon(){
+        this.telefon = telefon;
+    }
+    public void setAdres(){
+        this.adres=adres;
+    }
+    public void setMail(){
+        this.setMail();
+    }
+    public String getAdSoyad(){
+        return adSoyad;
+    }
+    public String getTelefon(){
+        return telefon;
+    }
+    public String getAdres(){
+        return adres;
+    }
+    public String getMail(){
+        return mail;
+    }
+
+
+}
