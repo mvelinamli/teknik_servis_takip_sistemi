00015df8d45284569394f6f81a0cf6c930531026
Ali <106073110+Zerothlaw780@users.noreply.github.com>
2025-11-02 17:08:10 +0300

    src klas sınıflar eklend 1

--- src/Cihaz.java
+++ src/Cihaz.java
@@ -1,4 +1,38 @@
 
 public class Cihaz {
+	public int cihazId;
+	public int sahipMusteriId;
+	public String seriNo;
+	public String markaModel;
+	public String arizaTanimi;
+	//SETTER PART//
+	public void setSeriNo(String SeriNo) {
+		String seriNo= SeriNo;
+	}
+	public void setMarkaModel(String MarkaModel) {
+		String markaModel= MarkaModel;
+	}
+	public void setArizaTanimi(String ArizaTanimi) {
+		String sahipMusteriId= seriNo;
+	}
+	//GET PART//
+	public void getCihazId() {
+		System.out.println(cihazId);
+	}
+	public void getSahipMusteriId() {
+		System.out.println(sahipMusteriId);
+	}
+	public void getSeriNo() {
+		System.out.println(seriNo);
+	}
+	public void getMarkaModel() {
+		System.out.println(markaModel);
+	}
+	public void getArizaTanimi() {
+		System.out.println(arizaTanimi);
+	}
+
+
+	
 
 }
