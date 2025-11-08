171cf5ea9b9075e61ab545e37ce1dea03a1acef0
Muhammed Veli NAMLI <162606231+mvelinamli@users.noreply.github.com>
2025-11-08 18:45:00 +0300

    Update ServisYoneticisi.java

--- src/ServisYoneticisi.java
+++ src/ServisYoneticisi.java
@@ -8,4 +8,31 @@ public class ServisYoneticisi {
 	public void cihazBul(int cihazId) {
 		
 	}
+	public void verileriYukle(String dosyaAdi){
+
+    }
+    public void verileriKaydet(String dosyaAdi){
+
+    }
+    public void musteriEkle(Musteri musteri){
+        
+    }
+    public void musteriBul(int musteriId){
+
+    }
+    public void musteriGuncelle(int musteriId){
+
+    }
+    public void yeniServisKaydiOlustur(ServisKaydi servisKaydi){
+
+    }
+    public void onarimdakiIsiGetir(){
+
+    }
+    public void servisDurumuGuncelle(int kayitId, String yeniDurum){
+
+    }
+    public void kayitlariFiltrele(String durum){
+
+    }
 }
