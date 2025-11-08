360c47a1af0209b1e8f09aad0e411af264d0eb39
Muhammed Veli NAMLI <muhammedveli.namli@icloud.com>
2025-11-08 19:52:54 +0300

    modified:   src/model/ServisYoneticisi.java

--- src/model/ServisYoneticisi.java
+++ src/model/ServisYoneticisi.java
@@ -1,5 +1,8 @@
 package model;
+
 public class ServisYoneticisi {
+    Musteri head = null;
+    Musteri tail = null;
 
     public void cihazEkle() {
         Cihaz cihaz = new Cihaz();
@@ -7,34 +10,58 @@ public class ServisYoneticisi {
         cihaz.setSeriNo(null);
         cihaz.setMarkaModel(null);
     }
+
     public void cihazBul(int cihazId) {
 
     }
-    public void verileriYukle(String dosyaAdi){
+
+    public void verileriYukle(String dosyaAdi) {
 
     }
-    public void verileriKaydet(String dosyaAdi){
+
+    public void verileriKaydet(String dosyaAdi) {
 
     }
-    public void musteriEkle(Musteri musteri){
+
+    public void musteriEkle(Musteri musteri) {
+        Node eleman = new Node();
+        eleman.data = x;
+
+        if (head == null) {
+            eleman.next = null;
+            head = eleman;
+            tail = eleman;
+            System.out.println("Liste yapisi olusturuldu ve eleman eklendi");
+        } else {
+            eleman.next = null;
+            tail.next = eleman;
+            tail = eleman;
+            System.out.println("Sona eleman eklendi");
+
+        }
+    }
+
+    public void musteriBul(int musteriId) {
 
     }
-    public void musteriBul(int musteriId){
+
+    public void musteriGuncelle(int musteriId) {
 
     }
-    public void musteriGuncelle(int musteriId){
+
+    public void yeniServisKaydiOlustur(ServisKaydi servisKaydi) {
 
     }
-    public void yeniServisKaydiOlustur(ServisKaydi servisKaydi){
+
+    public void onarimdakiIsiGetir() {
 
     }
-    public void onarimdakiIsiGetir(){
+
+    public void servisDurumuGuncelle(int kayitId, String yeniDurum) {
 
     }
-    public void servisDurumuGuncelle(int kayitId, String yeniDurum){
 
-    }
-    public void kayitlariFiltrele(String durum){
+    public void kayitlariFiltrele(String durum) {
 
     }
 }
