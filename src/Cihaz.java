b3621a89f8f0441eadd1acec678c26ac5e31b9f7
Muhammed Veli NAMLI <muhammedveli.namli@icloud.com>
2025-11-08 19:10:45 +0300

    modified:   src/Cihaz.java

--- src/Cihaz.java
+++ src/Cihaz.java
@@ -1,38 +1,44 @@
 
 public class Cihaz {
-	public int cihazId;
-	public int sahipMusteriId;
-	public String seriNo;
-	public String markaModel;
-	public String arizaTanimi;
-	//SETTER PART//
-	public void setSeriNo(String seriNo) {
-		this.seriNo= seriNo;
-	}
-	public void setMarkaModel(String markaModel) {
-		this.markaModel= markaModel;
-	}
-	public void setArizaTanimi(String arizaTanimi) {
-		this.arizaTanimi= arizaTanimi;
-	}
-	//GET PART//
-	public void getCihazId() {
-		System.out.println(cihazId);
-	}
-	public void getSahipMusteriId() {
-		System.out.println(sahipMusteriId);
-	}
-	public void getSeriNo() {
-		System.out.println(seriNo);
-	}
-	public void getMarkaModel() {
-		System.out.println(markaModel);
-	}
-	public void getArizaTanimi() {
-		System.out.println(arizaTanimi);
-	}
+    public int cihazId;
+    public int sahipMusteriId;
+    public String seriNo;
+    public String markaModel;
+    public String arizaTanimi;
 
+    //SETTER PART//
+    public void setSeriNo(String seriNo) {
+        this.seriNo = seriNo;
+    }
 
-	
+    public void setMarkaModel(String markaModel) {
+        this.markaModel = markaModel;
+    }
+
+    public void setArizaTanimi(String arizaTanimi) {
+        this.arizaTanimi = arizaTanimi;
+    }
+
+    //GET PART//
+    public void getCihazId() {
+        System.out.println(cihazId);
+    }
+
+    public void getSahipMusteriId() {
+        System.out.println(sahipMusteriId);
+    }
+
+    public void getSeriNo() {
+        System.out.println(seriNo);
+    }
+
+    public void getMarkaModel() {
+        System.out.println(markaModel);
+    }
+
+    public void getArizaTanimi() {
+        System.out.println(arizaTanimi);
+    }
+
 
 }
